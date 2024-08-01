707adc496da9abf3548f44ddb0fff1b3fd6b56b8
Erick Voyager <86367089+DevNinee@users.noreply.github.com>
2024-07-31 23:51:42 -0300

    Create chat.py

---
+++ chat.py
@@ -0,0 +1,18 @@
+# A chat game print the datas and likes to user and save variables
+
+username = input('What is your name? ' )
+
+print( "Hello" , username , "Nice to meet you" )
+
+user_age = input('how old are you? ' ),( username )
+
+print( "Oh, you are so young" ),( user_age )
+
+whatlike = input('what your favorite hobbie? ' )
+
+print( whatlike ,  "This things are so cool "  , username )
+
+something = input ('when do you play this? ' ) 
+
+print (  something , ", OK, i need to go now , see you later" , username )
+print ("wait wait" , username , "are you like to watch series and movies like ?  " )
